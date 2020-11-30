78c4265abab5aaf165406038f90a2d0f31565e95
P-SG <psgxx@ParkSangGun>
2020-11-30 12:30:22 +0900

    git 연동

---
+++ WebTest/src/test/DBConnection.java
@@ -0,0 +1,26 @@
+package test;
+ 
+import java.sql.Connection;
+import java.sql.SQLException;
+ 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+ 
+public class DBConnection 
+{
+    public static Connection getConnection() throws SQLException, NamingException, 
+    ClassNotFoundException{
+            Context initCtx = new InitialContext();
+            // 현재환경에서 naming context 획득하기
+            Context envCtx = (Context) initCtx.lookup("java:comp/env");
+            //DataSource 찾기
+            DataSource ds = (DataSource) envCtx.lookup("jdbc/orcl");
+            //Connection을  conn변수에 넣는다
+            Connection conn = ds.getConnection();
+            return conn; // Connection을 얻는다
+    }
+}
+
+
